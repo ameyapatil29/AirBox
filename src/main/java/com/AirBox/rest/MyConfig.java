79302894c847f07af291fa06e074462423e344bb
sumant murke <sumantmurke@gmail.com>
2014-04-15 18:18:39 -0700

    Initial Commit with one upload flow

---
+++ src/main/java/com/AirBox/rest/MyConfig.java
@@ -0,0 +1,26 @@
+package com.AirBox.rest;
+
+public class MyConfig {
+	
+public static String myAccessId;
+public static String mySecretId;
+public static String myBucketName;
+
+public static final String getMyAccessId() {
+	myAccessId= "AKIAINMLWZDSDIPO2PLQ";
+	return myAccessId;
+}
+
+public static final String getMySecretId() {
+	mySecretId="mDQCtJcJzABbrezBOMjxuhxZ+LdX52fVkEfWLzKW"; 
+	return mySecretId;
+}
+
+public static final String getMyBucketName() {
+	myBucketName= "cmpe273";
+	return myBucketName;
+}
+
+
+	
+}
