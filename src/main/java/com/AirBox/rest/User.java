b2ca1a948269520fc9f788c93b80c698bfa78628
chetanburande <chetan.burande7@gmail.com>
2014-04-15 20:29:04 -0700

    User and File domain files

---
+++ src/main/java/com/AirBox/rest/User.java
@@ -0,0 +1,60 @@
+package com.AirBox.rest;
+
+public class User {
+
+	private Integer id;
+	private String password;
+	private String userName;
+	private String firstName;
+	private String lastName;
+	//private String linkedInId;
+	//private String linkedInUrl;
+/*	
+	public String getLinkedInUrl() {
+		return linkedInUrl;
+	}
+	public void setLinkedInUrl(String linkedInUrl) {
+		this.linkedInUrl = linkedInUrl;
+	}
+	*/
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	/*
+	public String getLinkedInId() {
+		return linkedInId;
+	}
+	public void setLinkedInId(String linkedInId) {
+		this.linkedInId = linkedInId;
+	}
+	*/
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
+
